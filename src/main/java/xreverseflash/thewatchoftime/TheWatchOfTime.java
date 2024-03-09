f305969453ca12e6b3fbc410d60582b2f17159b3
xEobardThawne <66641809+xEobardThawne@users.noreply.github.com>
2024-03-09 13:56:07 -0600

    Optimized Code
    removed useless sound related code and all example mixin files

--- src/main/java/xreverseflash/thewatchoftime/TheWatchOfTime.java
+++ src/main/java/xreverseflash/thewatchoftime/TheWatchOfTime.java
@@ -20,15 +20,8 @@ public class TheWatchOfTime implements ModInitializer {
 	public static final TimeWatchItem TIME_WATCH =
 			Registry.register(Registries.ITEM, new Identifier("thewatchoftime", "time_watch"),
 					new TimeWatchItem(new FabricItemSettings().maxCount(1)));
-
-	public static final Identifier TIME_STOP_SOUND_ID = new Identifier("thewatchoftime:time_stop");
-	public static SoundEvent TIME_STOP_SOUND = SoundEvent.of(TIME_STOP_SOUND_ID);
-	public static final Identifier TIME_RESUME_SOUND_ID = new Identifier("thewatchoftime:time_resume");
-	public static SoundEvent TIME_RESUME_SOUND = SoundEvent.of(TIME_RESUME_SOUND_ID);
-
+	
 	@Override
 	public void onInitialize() {
-		Registry.register(Registries.SOUND_EVENT, TheWatchOfTime.TIME_STOP_SOUND_ID, TIME_STOP_SOUND);
-		Registry.register(Registries.SOUND_EVENT, TheWatchOfTime.TIME_RESUME_SOUND_ID, TIME_RESUME_SOUND);
 	}
 }
