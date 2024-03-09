86cb51e449abe74b1ff846948eae54cfdf356a00
xEobardThawne <66641809+xEobardThawne@users.noreply.github.com>
2024-03-09 13:56:31 -0600

    Added Comments

--- src/main/java/xreverseflash/thewatchoftime/TimeWatchItem.java
+++ src/main/java/xreverseflash/thewatchoftime/TimeWatchItem.java
@@ -16,11 +16,14 @@ public class TimeWatchItem extends Item {
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
+        // Get the world tick manager
         TickManager tickManager = world.getTickManager();
         if (!tickManager.isFrozen()) {
+            // If unfrozen, set to frozen
             tickManager.setFrozen(true);
             playerEntity.playSound(SoundEvents.BLOCK_METAL_PRESSURE_PLATE_CLICK_ON, 1.0F, 1.0F);
         } else {
+            // If frozen, set to unfrozen
             tickManager.setFrozen(false);
             playerEntity.playSound(SoundEvents.BLOCK_METAL_PRESSURE_PLATE_CLICK_OFF, 1.0F, 1.0F);
         }
