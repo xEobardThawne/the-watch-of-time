a44802ed32af04d8fdb0053d8979db9b1ae0fe31
xEobardThawne <66641809+xEobardThawne@users.noreply.github.com>
2024-10-28 23:26:10 -0500

    Fixed Code for 1.21

--- src/main/java/xreverseflash/thewatchoftime/TheWatchOfTime.java
+++ src/main/java/xreverseflash/thewatchoftime/TheWatchOfTime.java
@@ -2,24 +2,16 @@ package xreverseflash.thewatchoftime;
 
 import net.fabricmc.api.ModInitializer;
 
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.ServerTickManager;
-import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import static java.rmi.registry.Registry.*;
 
 public class TheWatchOfTime implements ModInitializer {
 
 	public static final TimeWatchItem TIME_WATCH =
-			Registry.register(Registries.ITEM, new Identifier("thewatchoftime", "time_watch"),
-					new TimeWatchItem(new FabricItemSettings().maxCount(1)));
+			Registry.register(Registries.ITEM, Identifier.of("thewatchoftime", "time_watch"),
+					new TimeWatchItem(new Item.Settings().maxCount(1)));
 	
 	@Override
 	public void onInitialize() {
